157e4a72e0c59d02ee7797c9f6027cd74c4b3dd2
patrick.liao <liao.patrick@gmail.com>
2024-08-07 00:57:05 -0700

    print roster

--- cmd/roster.go
+++ cmd/roster.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/patricka3125/pickle-bot/common"
 	"github.com/spf13/cobra"
@@ -20,16 +21,44 @@ var (
 				return err
 			}
 
-			fmt.Println(args[0])
 			roster, err := common.SignupRoster(args[0], data)
 			if err != nil {
 				return err
 			}
 
+			dateIn := prompt("Please enter date (MM/DD/YYYY)")
+			startTime := prompt("Please enter start time (HH:MM)")
+			endTime := prompt("Please enter end time (HH:MM)")
+			host := prompt("Please enter host uid (ou_xxx)")
+			payment := prompt("Please enter payment link")
+
+			date, err := time.Parse("01/02/2006", dateIn)
+			if err != nil {
+				return fmt.Errorf("invalid date: %w", err)
+			}
+
+			fmt.Println("\n------------------------------------------------------")
+			fmt.Printf("\nHost: %s\nPayment link: %s\n%s, %s %s - %s\n\n",
+				host, payment, date.Weekday().String(), dateIn, startTime, endTime)
 			for _, player := range roster {
 				fmt.Printf("player: %+v\n", player)
 			}
+
+			proceed := prompt("\nProceed with sending message? (y/n)")
+			if proceed != "y" {
+				fmt.Println("Aborting...")
+				return nil
+			}
+
 			return nil
 		},
 	}
 )
+
+func prompt(msg string) string {
+	var input string
+	fmt.Printf("%s : ", msg)
+	fmt.Scanf("%s", &input)
+
+	return input
+}
